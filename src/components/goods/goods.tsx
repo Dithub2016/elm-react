d1757df3595fca368f9eec324e24e37fc0b4a019
DK <903935008@qq.com>
2017-04-24 12:17:49 +0800

    fetch goods demo

--- src/components/goods/goods.tsx
+++ src/components/goods/goods.tsx
@@ -2,31 +2,23 @@ import * as React from 'react'
 import * as IScroll from '../../lib/iscroll.js'
 
 const css = require('./goods.sass');
-interface Test {
-    name: string,
-    list: Array<number>
-}
-const test:Test = {
-    name: 'abc',
-    list: [1, 2, 3]
-};
-const ratingList:Test[] = new Array(20).fill(test);
-const RatingTemp = (item: any[]) => {
+
+const goodList = (item: any[]) => {
     return (
         <ul className={css.goods_content}>
-            {item.map((el: number) => {
+            {item.map((el: any) => {
                 return (
                     <li className={css.good_detail}>
                         <div className={css.good_pic}>
-                            <img src={require("../../assets/avatar@2x.png")} alt=""/>
+                            <img src={el.image} alt=""/>
                         </div>
                         <div className={css.good_info}>
-                            <h1>皮蛋瘦肉粥配包子套餐</h1>
-                            <p className={css.good_species}>咸粥</p>
-                            <p className={css.good_sales}><span>月售1132份</span><span>好评率100%</span></p>
+                            <h1>{el.name}</h1>
+                            <p className={css.good_species}>{el.description}</p>
+                            <p className={css.good_sales}><span>月售{el.sellCount}份</span><span>好评率100%</span></p>
                             <div className={css.good_footer}>
                                 <div>
-                                    <span className={css.good_price}><i>￥</i>24</span>
+                                    <span className={css.good_price}><i>￥</i>{el.price}</span>
                                     <span className={css.good_price_before}>￥28</span>
                                 </div>
                                 <div className={css.btn}><span className={css.add}>+</span></div>
@@ -38,26 +30,46 @@ const RatingTemp = (item: any[]) => {
         </ul>
     )
 };
+let scrollRefs: any = [];
+interface JsonData  {
+    jsonData: any
+}
 export default class Goods extends React.Component<any, any> {
     scrollLeft: any;
     scrollRight: any;
-    componentDidMount () {
-        setTimeout(() => {
-            this.scrollLeft = new IScroll(this.refs.scrollLeft);
-            this.scrollRight = new IScroll(this.refs.scrollRight);
-        }, 100)
+    state: JsonData = {
+        jsonData: null
+    };
+    componentWillMount () {
+        fetch('http://localhost:8080/data.json')
+            .then((response) => {
+                return response.json()
+            })
+            .then((json) => {
+                this.setState({
+                    jsonData: json
+                });
+                setTimeout(() => {
+                    this.scrollLeft = new IScroll(this.refs.scrollLeft);
+                    this.scrollRight = new IScroll(this.refs.scrollRight);
+                }, 100)
+            });
+    }
+    scrollHandle (index: number) {
+        this.scrollRight.scrollToElement(this.refs[scrollRefs[index]]);
     }
     render () {
-        const Rating = ratingList.map((item: Test) => {
-            return <li>{item.name}</li>
+        const Rating = this.state.jsonData && this.state.jsonData.goods.map((item: any, index: number) => {
+            return <li onClick={this.scrollHandle.bind(this, index)}>{item.name}</li>
         });
-        const RatingEles = ratingList.map((item: Test) => {
+        const RatingHeader = this.state.jsonData && this.state.jsonData.goods.map((item: any, index: number) => {
+            scrollRefs.push('scrollRefs' + index);
             return (
                 <li>
-                    <div className={css.goods_header}>
+                    <div className={css.goods_header + " scroll-refs"} ref={scrollRefs[index]}>
                         {item.name}
                     </div>
-                    {RatingTemp(item.list)}
+                    {goodList(item.foods)}
                 </li>
             )
         });
@@ -70,10 +82,12 @@ export default class Goods extends React.Component<any, any> {
                 </div>
                 <div className={css.goods_list} ref="scrollRight">
                     <ul>
-                        {RatingEles}
+                        {RatingHeader}
                     </ul>
                 </div>
             </div>
         )
     }
 }
+
+
